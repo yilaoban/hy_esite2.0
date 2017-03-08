3f8b685b1f41926cfd95985d07c4f454e791314f
huazhongming <huazhongming@huazhongming.yinpiao.com>
2017-03-08 14:32:27 +0800

    111

--- src/com/huiyee/bdmap/action/BaiDuAction.java
+++ src/com/huiyee/bdmap/action/BaiDuAction.java
@@ -20,7 +20,7 @@ public class BaiDuAction extends AbstractAction
 {
 
 	/**
-	 * 百度地图 通过关键字搜索地址列表.测试github2
+	 * 百度地图 通过关键字搜索地址列表.测试github2,111
 	 */
 	private static final long serialVersionUID = 3887688790629684079L;
 	private String q = "";
